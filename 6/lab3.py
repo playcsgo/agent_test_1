3ff24133f700846908ca77fd122d11f8a271861c
playcsgo <formitfina@gmail.com>
2026-03-20 17:17:14 +0800

    add polygon for stock price

--- 6/lab3.py
+++ 6/lab3.py
@@ -14,7 +14,12 @@ def build_mcp_params() -> dict[str, MCPServerStdioParams]:
         'introduction': MCPServerStdioParams(
             command='npx',
             args=["-y", "mcp-memory-libsql"],
-            env={"LIBSQL_URL": "file:./memory/ed.db"} # 直接指定 LIBSQL_URL = file:./memory/ed.db
+            env={**os.environ, "LIBSQL_URL": "file:./memory/ed.db"} # 直接指定 LIBSQL_URL = file:./memory/ed.db
+        ),
+        'stocker_price_searcher': MCPServerStdioParams(
+            command='uv',
+            args=["run", "market_server.py"],
+            env={**os.environ}
         )
     }
 
@@ -31,5 +36,34 @@ async def run_local_mcp():
         print('Result_1:', result.final_output)
 
 
+
+## polygon (Massive)
+polygon_api_key = os.getenv("POLYGON_API_KEY")
+if not polygon_api_key:
+    print('POLYGON_API_KEY not found !')
+
+from polygon import RESTClient
+client = RESTClient(polygon_api_key)
+result = client.get_previous_close_agg("AAPL")
+
+
+from market import get_share_price
+# get_share_price('AAPL')
+
+async def check_stock_price():
+    INSTRUCTIONS = "You answer questions about the stock market."
+    request = "What's the share price of Apple?"
+    LLM_MODEL = 'gpt-4.1-mini'
+    mcp_params = build_mcp_params()
+    async with MCPServerStdio(params=mcp_params['stocker_price_searcher'], client_session_timeout_seconds=30) as mcp_server:
+        agent = Agent(name='agent', instructions=INSTRUCTIONS, model=LLM_MODEL, mcp_servers=[mcp_server])
+        with trace('check_price'):
+            result = await Runner.run(agent, request)
+        print('check_price:', result.final_output)
+
+
+
 if __name__ == '__main__':
-    asyncio.run(run_local_mcp())
+    # asyncio.run(run_local_mcp())
+    asyncio.run(check_stock_price())
+
